be1e309857183338ce63dc2e1c332a73a50ab11f
Rolly Maulana Awangga <rolly@awang.ga>
2023-07-02 23:52:37 +0700

    y

--- url/url.go
+++ url/url.go
@@ -11,9 +11,4 @@ func Web(page *fiber.App) {
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsaut
 	page.Get("/", controller.GetHome)
-	page.Get("/tam", controller.GetTam)
-	page.Get("/undangan", controller.GetDataUndanganRapat)
-	page.Get("/peserta", controller.GetDataPesertaRapat)
-	page.Get("/waktu", controller.GetDataWaktuRapat)
-	page.Get("/rapat", controller.GetDataRapatMulai)
 }
